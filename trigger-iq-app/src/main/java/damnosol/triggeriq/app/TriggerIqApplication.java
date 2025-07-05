50ad0610a50c20f698c443806f94590ebdb0c53b
jovanmontfort <jo.vanmontfort@damno-solutions.be>
2025-07-05 15:50:21 +0200

    com -> damnosol

---
+++ trigger-iq-app/src/main/java/damnosol/triggeriq/app/TriggerIqApplication.java
@@ -0,0 +1,11 @@
+package damnosol.triggeriq.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriggerIqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TriggerIqApplication.class, args);
+    }
+}
